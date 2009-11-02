3bd77d715bcb46a5a5e76bf6184e7edb5fcf99fb
Rob Rix <rob@monochromeindustries.com>
2009-11-02 01:23:51 -0500

    Initial import with some notes about what it is, what it’s for, and some questions about implementing it.

---
+++ RXVersionedUndoManager.h
@@ -0,0 +1,13 @@
+// RXVersionedUndoManager.h
+// Created by Rob Rix on 2009-11-02
+// Copyright 2009 Monochrome Industries
+
+#import <Foundation/Foundation.h>
+
+@interface RXVersionedUndoManager : NSUndoManager {
+	
+}
+
+
+
+@end
---
+++ RXVersionedUndoManager.m
@@ -0,0 +1,11 @@
+// RXVersionedUndoManager.m
+// Created by Rob Rix on 2009-11-02
+// Copyright 2009 Monochrome Industries
+
+#import "RXVersionedUndoManager.h"
+
+@implementation RXVersionedUndoManager
+
+
+
+@end
